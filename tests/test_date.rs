a89970c4daacb4d4950756efefdc47eb88990363
Sylvestre Ledru <sylvestre@debian.org>
2020-05-02 13:27:13 +0200

    date: add more tests

--- tests/test_date.rs
+++ tests/test_date.rs
@@ -1,4 +1,9 @@
+extern crate regex;
+
 use common::util::*;
+use self::regex::Regex;
+
+
 
 #[test]
 fn test_date_email() {
@@ -16,9 +21,26 @@ fn test_date_email2() {
 
 #[test]
 fn test_date_rfc_3339() {
-    let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("--rfc-3339=ns").run();
-    assert!(result.success);
+    let scene = TestScenario::new(util_name!());
+
+    let mut result = scene
+    .ucmd()
+    .arg("--rfc-3339=ns")
+    .succeeds();
+
+    // Check that the output matches the regexp
+    let rfc_regexp = r"(\d+)-(0[1-9]|1[012])-(0[1-9]|[12]\d|3[01])\s([01]\d|2[0-3]):([0-5]\d):([0-5]\d|60)(\.\d+)?(([Zz])|([\+|\-]([01]\d|2[0-3])))";
+    let re = Regex::new(rfc_regexp).unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+    result = scene
+    .ucmd()
+    .arg("--rfc-3339=seconds")
+    .succeeds();
+
+    // Check that the output matches the regexp
+    let re = Regex::new(rfc_regexp).unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
 }
 
 #[test]
@@ -51,7 +73,96 @@ fn test_date_universal() {
 
 #[test]
 fn test_date_format_y() {
-    let (_, mut ucmd) = at_and_ucmd!();
-    let result = ucmd.arg("+%y").run();
+    let scene = TestScenario::new(util_name!());
+
+    let mut result = scene
+    .ucmd()
+    .arg("+%Y")
+    .succeeds();
+
     assert!(result.success);
+    let mut re = Regex::new(r"^\d{4}$").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+    result = scene
+    .ucmd()
+    .arg("+%y")
+    .succeeds();
+
+    assert!(result.success);
+    re = Regex::new(r"^\d{2}$").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+}
+
+
+#[test]
+fn test_date_format_m() {
+    let scene = TestScenario::new(util_name!());
+
+    let mut result = scene
+    .ucmd()
+    .arg("+%b")
+    .succeeds();
+
+    assert!(result.success);
+    let mut re = Regex::new(r"\S+").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+    result = scene
+    .ucmd()
+    .arg("+%m")
+    .succeeds();
+
+    assert!(result.success);
+    re = Regex::new(r"^\d{2}$").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+}
+
+#[test]
+fn test_date_format_day() {
+    let scene = TestScenario::new(util_name!());
+
+    let mut result = scene
+    .ucmd()
+    .arg("+%a")
+    .succeeds();
+
+    assert!(result.success);
+    let mut re = Regex::new(r"\S+").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+    result = scene
+    .ucmd()
+    .arg("+%A")
+    .succeeds();
+
+    assert!(result.success);
+
+    re = Regex::new(r"\S+").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+
+    result = scene
+    .ucmd()
+    .arg("+%u")
+    .succeeds();
+
+    assert!(result.success);
+    re = Regex::new(r"^\d{1}$").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
+}
+
+#[test]
+fn test_date_format_full_day() {
+    let scene = TestScenario::new(util_name!());
+
+    let result = scene
+    .ucmd()
+    .arg("+'%a %Y-%m-%d'")
+    .succeeds();
+
+    assert!(result.success);
+    let re = Regex::new(r"\S+ \d{4}-\d{2}-\d{2}").unwrap();
+    assert!(re.is_match(&result.stdout.trim()));
 }
