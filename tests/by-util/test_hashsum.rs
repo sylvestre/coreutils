af7df1aece0f0ce8979f19b539f34186588c7dc2
Sylvestre Ledru <sylvestre@debian.org>
2024-05-18 21:53:45 +0200

    hashsum: add a test

--- tests/by-util/test_hashsum.rs
+++ tests/by-util/test_hashsum.rs
@@ -268,6 +268,28 @@ fn test_check_b2sum_tag_output() {
 }
 
 #[test]
+fn test_check_b2sum_verify() {
+    let scene = TestScenario::new(util_name!());
+    let at = &scene.fixtures;
+
+    at.write("a", "a\n");
+
+    scene
+        .ccmd("b2sum")
+        .arg("-c")
+        .arg("a")
+        .succeeds()
+        .stdout_only("BLAKE2b-128 (a) = b93e0fc7bb21633c08bba07c5e71dc00\n");
+
+    scene
+        .ccmd("b2sum")
+        .arg("-c")
+        .arg("a")
+        .succeeds()
+        .stdout_only("BLAKE2b (a) = bedfbb90d858c2d67b7ee8f7523be3d3b54004ef9e4f02f2ad79a1d05bfdfe49b81e3c92ebf99b504102b6bf003fa342587f5b3124c205f55204e8c4b4ce7d7c\n");
+}
+
+#[test]
 fn test_check_file_not_found_warning() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
